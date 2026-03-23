5a077b2258a4ff6eff80a0daa60bdbfbde89a3eb
Vasilii Cojocari <47657115+cojocariv@users.noreply.github.com>
2026-03-23 18:54:47 +0200

    Refactor homepage product display logic: removed unused image assignment code for featured products to streamline the index method in HomeController.

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,16 +6,6 @@ class HomeController extends Controller
     public function index(): void
     {
         $products = (new Product())->featured(4);
-        $usiUrls = asset_gallery_images('usi');
-        $n = count($usiUrls);
-        $i = 0;
-        foreach ($products as &$p) {
-            if ($n > 0) {
-                $p['display_image'] = $usiUrls[$i % $n];
-            }
-            $i++;
-        }
-        unset($p);
 
         $projects = array_slice((new Project())->all(), 0, 6);
         $this->render('pages/home', [
