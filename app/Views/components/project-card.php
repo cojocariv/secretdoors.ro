86368487ea6928a777481e881438be8508a6e581
Vasilii Cojocari <47657115+cojocariv@users.noreply.github.com>
2026-04-10 10:52:44 +0300

    Refactor project card image URLs: removed query parameters from Azure Blob storage URLs for improved stability. Updated image source handling to utilize a normalization function, enhancing reliability in image loading.

--- app/Views/components/project-card.php
+++ app/Views/components/project-card.php
@@ -3,11 +3,11 @@
 $show_project_image_carousel = $show_project_image_carousel ?? false;
 
 $carouselImages = [
-    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_7929.JPG?sp=racwdl&st=2026-03-23T17:46:49Z&se=2026-04-10T01:01:49Z&sv=2024-11-04&sr=c&sig=p%2FEGIMcr6%2BTlfBYLMP6cuEpsCQTEJjUf8FwVc%2BT0n58%3D',
-    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8019.JPG?sp=racwdl&st=2026-03-23T17:46:49Z&se=2026-04-10T01:01:49Z&sv=2024-11-04&sr=c&sig=p%2FEGIMcr6%2BTlfBYLMP6cuEpsCQTEJjUf8FwVc%2BT0n58%3D',
-    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8072.JPG?sp=racwdl&st=2026-03-23T17:46:49Z&se=2026-04-10T01:01:49Z&sv=2024-11-04&sr=c&sig=p%2FEGIMcr6%2BTlfBYLMP6cuEpsCQTEJjUf8FwVc%2BT0n58%3D',
-    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8073.JPG?sp=racwdl&st=2026-03-23T17:46:49Z&se=2026-04-10T01:01:49Z&sv=2024-11-04&sr=c&sig=p%2FEGIMcr6%2BTlfBYLMP6cuEpsCQTEJjUf8FwVc%2BT0n58%3D',
-    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8074.JPG?sp=racwdl&st=2026-03-23T17:46:49Z&se=2026-04-10T01:01:49Z&sv=2024-11-04&sr=c&sig=p%2FEGIMcr6%2BTlfBYLMP6cuEpsCQTEJjUf8FwVc%2BT0n58%3D',
+    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_7929.JPG',
+    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8019.JPG',
+    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8072.JPG',
+    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8073.JPG',
+    'https://cojocaristorage.blob.core.windows.net/secretdoors/usi/IMG_8074.JPG',
 ];
 ?>
 
@@ -16,7 +16,7 @@ $carouselImages = [
         <div class="relative h-56 w-full overflow-hidden" data-project-carousel data-interval="3000">
             <?php foreach ($carouselImages as $i => $imgUrl): ?>
                 <img
-                    src="<?= e($imgUrl) ?>"
+                    src="<?= e(normalize_image_url($imgUrl)) ?>"
                     alt="<?= e($project['title'] ?? 'Proiect') ?>"
                     loading="<?= $i === 0 ? 'eager' : 'lazy' ?>"
                     class="js-lightbox project-carousel-slide project-carousel-anim absolute inset-0 h-56 w-full object-cover cursor-zoom-in <?= $i === 0 ? 'opacity-100' : 'opacity-0' ?>"
